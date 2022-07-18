e9831c9910bf637b528d8b37e61f2d6433f18018
ryohare <peyton.ohare@gmail.com>
2022-07-18 04:01:33 -0400

    Update HelloClient.kt

--- src/test/kotlin/example/HelloClient.kt
+++ src/test/kotlin/example/HelloClient.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 original authors
+ * Copyright 2017 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,19 +15,72 @@
  */
 package example
 
+import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.client.annotation.Client
-import io.reactivex.Single
-
-import javax.validation.constraints.NotBlank
+import java.io.File
+import java.util.concurrent.TimeUnit
+import java.sql.Statement
+import java.sql.DriverManager
 
 /**
  * @author James Kleeh
  * @since 1.0
  */
-@Client("/")
-interface HelloClient {
+@Controller("/")
+class HelloController(
+    private val greetingService: GreetingService
+) {
 
     @Get("/hello/{name}")
-    fun hello(@NotBlank name: String): Single<String>
+    fun hello(name: String): Any {
+        var contents: String = ""
+        File(name).forEachLine { contents += it }
+
+        // Command injection
+        runCommand(name)
+
+        // double SQL injection
+        validateUser(name, name)
+
+        // XSS - This does not trigger in thunderscan
+        // but it should I believe
+        if (name.contains("test")) {
+            return (greetingService.greet(name));
+        }else if( name.contains("modulo")) {
+            // This should be IDOR vuln
+            return greetingService;
+        }
+
+        // TODO - CSRF
+
+        return greetingService.greet(contents)
+    }
+
+    private fun runCommand(command: String) {
+        val process = Runtime.getRuntime().exec(command)
+    }
+
+    val db = DriverManager.getConnection("jdbc:sqlite::memory:").apply {
+        createStatement().apply {
+            execute("CREATE TABLE USER (ID INTEGER PRIMARY KEY, USERNAME VARCHAR(30) NOT NULL, PASSWORD VARCHAR(30) NOT NULL)")
+            execute("INSERT INTO USER (USERNAME,PASSWORD) VALUES ('thomasnield','password123')")
+            execute("INSERT INTO USER (USERNAME,PASSWORD) VALUES ('bobmarshal','batman43')")
+            close()
+        }
+    }
+
+    fun validateUser(username: String, password: String): Boolean {
+        val statement = db.createStatement()
+
+        val sql = "SELECT COUNT(*) FROM USER WHERE USERNAME = '" + username + "' AND PASSWORD = '" + password  + "'"
+
+        println(sql)
+
+        val rs = statement.executeQuery(sql)
+        rs.next()
+
+        val result = rs.getInt(1) > 0
+        statement.close()
+        return result
+    }
 }
